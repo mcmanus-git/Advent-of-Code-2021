14a74520c6e9f439856092b3b31299886a3c0f12
mcmanus-git <melancholic_maclean@yahoo.com>
2021-12-19 23:46:36 -0500

    day one part one solution

--- day_01/day_01.py
+++ day_01/day_01.py
@@ -1,1 +1,5 @@
-print('hello')
+with open('input.txt', 'r') as file:
+    depths = file.readlines()
+    depths = [int(l) for l in depths]
+
+print(sum([1 for i, depth in enumerate(depths) if depth - depths[i - 1] > 0]))
