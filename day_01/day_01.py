9612e7e7aa19d57712cb32b58ec4d13ae94e1cad
Michael M <melancholic_maclean@yahoo.com>
2021-12-21 21:01:52 -0500

    added day one part two

--- day_01/day_01.py
+++ day_01/day_01.py
@@ -1,5 +1,8 @@
 with open('input.txt', 'r') as file:
-    depths = file.readlines()
-    depths = [int(l) for l in depths]
+    depths = [int(l) for l in file.readlines()]
 
+# Part 1
 print(sum([1 for i, depth in enumerate(depths) if depth - depths[i - 1] > 0]))
+
+# Part 2
+print(sum([1 for i, depth in enumerate(depths) if (sum(depths[i-3: i]) - sum(depths[i-4:i-1])) > 0]))
